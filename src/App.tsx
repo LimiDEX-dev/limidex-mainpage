98f07f002c292e7d7071d8b17edef7a0d2a96e6a
SergeyBeltsin <beltsins89@gmail.com>
2022-02-15 23:32:26 +0300

    make reveal and static video

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './App.css';
 import { SocialList } from './shared/ui/social-list';
 import { Navigation } from './shared/ui/navigation';
+import { Background } from './shared/ui/background';
 
-function App() {
-  return (
-    <div className="App">
-      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video src="/assets/background.mp4" autoPlay loop muted />
-      <Navigation />
-      <SocialList />
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="App">
+    <Background />
+    <Navigation />
+    <SocialList />
+  </div>
+);
---
+++ src/shared/ui/background/index.tsx
@@ -0,0 +1,75 @@
+import {
+  FC,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
+import s from './style.module.css';
+
+export const Background: FC = () => {
+  const [isRevealEnded, setIsRevealEnded] = useState<boolean>(false);
+  const [isVerticalVideo, setIsVerticalVideo] = useState<boolean>(false);
+  const revealVideoRef = useRef<HTMLVideoElement>(null);
+  const staticVideoRef = useRef<HTMLVideoElement>(null);
+
+  useLayoutEffect(() => {
+    const handleVideoEnded = () => {
+      setIsRevealEnded(true);
+    };
+    revealVideoRef.current?.addEventListener('ended', handleVideoEnded);
+
+    return () => {
+      revealVideoRef.current?.removeEventListener('ended', handleVideoEnded);
+    };
+  }, [revealVideoRef.current]);
+
+  useLayoutEffect(() => {
+    const handleResize = (event: Event) => {
+      const window = event.target as Window;
+
+      if (window.innerWidth >= window.innerHeight) {
+        setIsVerticalVideo(false);
+        return;
+      }
+
+      setIsVerticalVideo(true);
+    };
+    window.addEventListener('resize', handleResize);
+
+    if (window.innerWidth >= window.innerHeight) {
+      setIsVerticalVideo(false);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getVideoDist = (fileName: string) => {
+    if (isVerticalVideo) {
+      return `${fileName}_vert`;
+    }
+
+    return fileName;
+  };
+
+  return (
+    <div className={`${s.container} ${isRevealEnded ? s.static : ''}`}>
+      <video
+        src={`/assets/${getVideoDist('background-static')}.mp4`}
+        loop
+        autoPlay
+        muted
+        ref={staticVideoRef}
+        className={s.video}
+      />
+      <video
+        src={`/assets/${getVideoDist('background-reveal')}.mp4`}
+        autoPlay
+        muted
+        ref={revealVideoRef}
+        className={s.video}
+      />
+    </div>
+  );
+};
